b7f01b402da539d44984830f3b6103321d93cbdd
VnQuynh810 <161539796+VnQuynh810@users.noreply.github.com>
2026-04-22 22:00:14 +0700

    Downgrade Gradle, AGP, and Android SDK versions
    - Downgraded Gradle wrapper from 9.3.1 to 8.10.2.
    - Updated Android Gradle Plugin (AGP) from 9.1.1 to 8.8.0.
    - Reverted `compileSdk` and `targetSdk` from 36 to 35.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.fitflow"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.fitflow"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
