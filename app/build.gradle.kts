0f35dd52cd8cd79edc66648040fd231aa4f00bfa
wind5293 <mywind5293@gmail.com>
2026-06-07 18:43:25 +0700

    feat(chatbot): add api key for chatbot reply (not working, needed test)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("io.coil-kt:coil-gif:2.6.0")
     implementation("sh.calvin.reorderable:reorderable:2.4.3")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
